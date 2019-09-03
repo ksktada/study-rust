9d67396ec99dc1ae35054668dfc65e12877acf75
Keisuke TADA <ksk.tada@gmail.com>
2019-09-03 20:53:13 +0900

    Add files via upload

--- guessing_game/main.rs
+++ guessing_game/main.rs
@@ -1,7 +1,13 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("Please input your guess.");
 
@@ -10,4 +16,12 @@ fn main() {
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less    => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal   => println!("You win!"),
+    }
 }
