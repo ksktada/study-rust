bdc93240dcb7c006c21cde867b448a7332a3fa73
Keisuke TADA <ksk.tada@gmail.com>
2019-09-11 21:23:52 +0900

    Update main.rs

--- data_types/main.rs
+++ data_types/main.rs
@@ -2,6 +2,20 @@ fn main() {
     let x = 2.0; // f64
     let y: f32 = 3.0; // f32
     
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+
+    // remainder
+    let remainder = 43 % 5;
     let tup = (500, 6.4, 1);
 
     let (x, y, z) = tup;
