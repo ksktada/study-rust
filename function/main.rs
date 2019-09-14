70b1fcf3429707a22ebfc4f83946b605164d6f01
Keisuke TADA <ksk.tada@gmail.com>
2019-09-14 13:04:14 +0900

    Update main.rs

--- function/main.rs
+++ function/main.rs
@@ -1,7 +1,14 @@
 fn main() {
     print_number(5);
+    
+    another_function(5, 6);
 }
 
 fn print_number(x: i32) {
     println!("x is: {}", x);
+}
+
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
