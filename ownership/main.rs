bfe8f5d7d2af41f8e9879f203abeef09d9c50850
Keisuke TADA <ksk.tada@gmail.com>
2019-09-21 23:55:28 +0900

    Update main.rs

--- ownership/main.rs
+++ ownership/main.rs
@@ -3,6 +3,12 @@ fn main() {
   let s2 = s1.clone();
 
   println!("s1 = {}, s2 = {}", s1, s2);
+
+  let x = 5;
+  let y = x;
+
+  println!("x = {}, y = {}", x, y);
+
 }
 // This code does not compile
 // s1 doesn't have ownership.
