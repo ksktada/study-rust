5cf7de9032c04ab20dbb5e21729e7c53e55ace88
Keisuke TADA <ksk.tada@gmail.com>
2019-09-24 23:44:40 +0900

    Update main.rs

--- structure/main.rs
+++ structure/main.rs
@@ -12,3 +12,20 @@ let user1 = User {
     sign_in_count: 1,
 };
 
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
