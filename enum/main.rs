3ec2e149a2e82ad1bd98990d59bdbc36e8b4f169
ksktada <ksk.tada@gmail.com>
2021-01-21 23:59:59 +0900

    enum

--- enum/main.rs
+++ enum/main.rs
@@ -31,11 +31,46 @@
 // let loopback = IpAddr::V6(String::from("::1"));
 
 // # pattern 3
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+// # pattern 4
+// standard library?
+
+// struct Ipv4Addr {
+//     // --snip--
+// }
+
+// struct Ipv6Addr {
+//     // --snip--
+// }
+
+// enum IpAddr {
+//     V4(Ipv4Addr),
+//     V6(Ipv6Addr),
+// }
+
+// Message
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String)
+    ChangeColor(i32, i32, i32)
+}
+
+// ↑can describe same object↓
+struct QuitMessage; // unit struct 
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String) // tuple struct
+struct ChangeColor(i32, i32, i32) // tuple struct
+
 fn main() {
-    enum IpAddr {
-        V4(u8, u8, u8, u8),
-        V6(String),
-    }
+
 
     let home = IpAddr::V4(127, 0, 0, 1);
 
