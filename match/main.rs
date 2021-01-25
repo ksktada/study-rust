e4728fab01606d81ccc1f2394d302b8f89b50f16
ksktada <ksk.tada@gmail.com>
2021-01-25 23:59:59 +0900

    match

--- match/main.rs
+++ match/main.rs
@@ -1,8 +1,13 @@
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
 enum Coin {
     Penny,
     Nikel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -10,6 +15,9 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Penny -> 1,
         Coin::Nickel -> 5,
         Coin::Dime -> 10,
-        Coin::Quarter -> 25,
+        Coin::Quarter(UsState) => {
+            println("State quarter from {:?}", state);
+            25
+        }
     }
 }
