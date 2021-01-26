727737c41e32c83db74efad7eca6683a54629006
ksktada <ksk.tada@gmail.com>
2021-01-26 23:59:59 +0900

    if let

--- match/main.rs
+++ match/main.rs
@@ -20,4 +20,23 @@ fn value_in_cents(coin: Coin) -> u8 {
             25
         }
     }
+
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1
+    }
+}
+
+fn if_let() {
+    let some_u8_value = Some(0u8)
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    if let Some(3) = some_u8_value {
+        println!("three")
+    }
 }
